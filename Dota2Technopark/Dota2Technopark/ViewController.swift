438fce81d8c3f956f0e3a678d8f55d45ea41658b
Kirill Davydenkov <kirilldavy18@gmail.com>
2024-03-05 01:19:52 +0300

    test commit

--- Dota2Technopark/Dota2Technopark/ViewController.swift
+++ Dota2Technopark/Dota2Technopark/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController {
         setupContainerView()
     }
     
+    
+    
     @objc func showSuccessAlert() {
         let alert = UIAlertController(title: "Success", message: "Welcome \(steamUser?.steamVanityID ?? "").\n  Your steamID32 is : \(steamUser?.steamID32 ?? ""). \n Your steamID64 is : \(steamUser?.steamID64 ?? "").", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { (action) in
