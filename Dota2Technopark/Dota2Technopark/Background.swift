bd3d5186410deced2ee4dd66ae52465cfc34d55f
Mikhail Ulanov <mixaulanoff@yandex.ru>
2024-03-06 17:09:48 +0300

    add a animated background

--- Dota2Technopark/Dota2Technopark/Background.swift
+++ Dota2Technopark/Dota2Technopark/Background.swift
@@ -7,28 +7,71 @@
 
 import SwiftUI
 
-struct BackgroundView: View {
-    let backgroundGradient = LinearGradient(
-        colors: [Color.red, Color.blue],
-        startPoint: .top, endPoint: .bottom)
+
+struct AnimatedGradientViewLight: View {
+    @Environment(\.colorScheme) var colorScheme
+    @State var gradientColors = [Color.purple, Color.mint] //Light
+    let durationTime: Int
     
     var body: some View {
-        ZStack {
-            backgroundGradient
-                .ignoresSafeArea()
-            VStack {
-                Spacer()
-                HStack {
-                    Image("background1")
-                        .resizable()
-                        .frame(width: 500, height: 300)
-                        .background(Color.clear)
+        LinearGradient(gradient: Gradient(colors: gradientColors), startPoint: .top, endPoint: .bottom)
+            .animation(Animation.linear(duration: TimeInterval(durationTime)).repeatForever())
+            .onAppear {
+                withAnimation{
+                    determineColorsForCurrentTheme()
                 }
+            }
+    }
+    private func determineColorsForCurrentTheme() {
+        withAnimation {
+            gradientColors = [Color.mint, Color.purple]
+            }
+        }
+}
+
+
+
+struct AnimatedGradientViewDark: View {
+    @Environment(\.colorScheme) var colorScheme
+    @State var gradientColors = [Color.cyan, Color.black] //Dark
+    let durationTime: Int
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: gradientColors), startPoint: .top, endPoint: .bottom)
+            .animation(Animation.linear(duration: TimeInterval(durationTime)).repeatForever())
+            .onAppear {
+                withAnimation{
+                    determineColorsForCurrentTheme()
+                }
+            }
+    }
+    private func determineColorsForCurrentTheme() {
+        withAnimation {
+            if colorScheme == .dark {
+                gradientColors = [Color.black, Color.cyan] // Цвета для темной темы
             }
         }
     }
 }
 
+
+struct BackgroundView: View {
+    @Environment(\.colorScheme) var colorScheme
+    let durationTime = 5
+    var body: some View {
+        ZStack {
+            if colorScheme == .light {
+                AnimatedGradientViewLight(durationTime: durationTime)
+                    .ignoresSafeArea()
+            } else {
+                AnimatedGradientViewDark(durationTime: durationTime)
+                    .ignoresSafeArea()
+            }
+            Image("background1")
+                .resizable()
+                .frame(width: 400, height: 222)
+        }
+    }
+}
 
 #Preview {
     BackgroundView()
