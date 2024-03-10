b24e8a730d628833fe69a4d3209f8fe9a8b5a969
dongrigory <dongrigory29@gmail.com>
2024-03-11 02:36:25 +0300

    Update animation and add button

--- Dota2Technopark/Dota2Technopark/Modules/AuthorizationModule/View/AuthView.swift
+++ Dota2Technopark/Dota2Technopark/Modules/AuthorizationModule/View/AuthView.swift
@@ -17,10 +17,6 @@ class AuthView: UIViewController {
     
     var output: AuthViewOutput?
     
-    override func viewDidAppear(_ animated: Bool) {
-//        output?.getSteamUser()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -37,20 +33,53 @@ class AuthView: UIViewController {
         }))
         self.present(alert, animated: true, completion: nil)
     }
+    
+    @objc func buttonTapped() {
+        UIView.animate(withDuration: 0.1,
+                       animations: {
+            self.steamButton.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        },
+                       completion: { _ in
+            UIView.animate(withDuration: 0.1) {
+                self.steamButton.transform = CGAffineTransform.identity
+            }
+        })
+        
+        output?.getSteamUser()
+    }
+
 }
 
 extension AuthView {
     func setupUI() {
         self.view.backgroundColor = .white
         setupSteamLogo()
+        setupButton()
     }
     
     func setupButton() {
-        
+        steamButton = UIButton(type: .system)
+        steamButton.setTitle("Войти через Steam!", for: .normal)
+        steamButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        steamButton.setTitleColor(.white, for: .normal)
+        steamButton.backgroundColor = UIColor(hex: "82929E")
+
+        steamButton.layer.cornerRadius = 25
+
+        steamButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+
+        self.view.addSubview(steamButton)
+
+        steamButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(steamLogoView.snp.bottom).offset(30)
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+        }
     }
     
     func setupSteamLogo() {
-        steamLogoView = LottieAnimationView(name: "steam")
+        steamLogoView = LottieAnimationView(name: "Hello")
         steamLogoView.translatesAutoresizingMaskIntoConstraints = false
         
         self.view.addSubview(steamLogoView)
@@ -69,7 +98,7 @@ extension AuthView {
 
 extension AuthView: AuthViewInput {
     func performUser(steamUser: SteamUser?) {
-        if steamUser != nil {
+        if steamUser == nil {
             SteamLoginVC.login(from: self) { [weak self] (user, error) in
                 guard let self = self else { return }
                 if let user = user {
