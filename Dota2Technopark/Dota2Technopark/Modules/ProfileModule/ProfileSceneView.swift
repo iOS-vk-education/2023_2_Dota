d584e5794f3b674e4364c74068b401c1f33ea141
Kirill Davydenkov <kirilldavy18@gmail.com>
2024-03-11 23:04:43 +0300

    update UI and minor fixes add assets

--- Dota2Technopark/Dota2Technopark/Modules/ProfileModule/ProfileSceneView.swift
+++ Dota2Technopark/Dota2Technopark/Modules/ProfileModule/ProfileSceneView.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct ProfileSceneView: View {
     var body: some View {
-        ZStack {
-            BackgroundView(imageOnBackground: false, durationTime: 10)
-            VStack {
-                ProfileInformationView()
-                NavigationView()
-                Spacer()
+        NavigationStack {
+            ZStack {
+                BackgroundView(imageOnBackground: false, durationTime: 10)
+                VStack {
+                    ProfileInformationView()
+                    NavigationListView()
+                    Spacer()
+                }
             }
         }
     }
@@ -24,161 +26,91 @@ struct ProfileInformationView: View {
     var body: some View {
         VStack {
             HStack(alignment: .top, spacing: 15, content: {
-                RoundedRectangle(cornerRadius: 25)
-                    .foregroundStyle(.white)
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(.white.gradient)
                     .shadow(color: Color.gray, radius: 10, x: 0, y: 10)
                     .overlay {
                         VStack {
                             Image("avatar")
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
-                                .padding(10)
+                                .clipShape(RoundedRectangle(cornerRadius: 16))
+                                .padding(8)
+                                .padding(.top, -16)
                             Text("TSpirit.Yatoro")
                                 .font(.system(size: 25))
+                                .minimumScaleFactor(0.5)
+                                .lineLimit(1)
+                                .padding(.horizontal, 8)
                         }
                     }
                 VStack {
-                    RoundedRectangle(cornerRadius: 25)
-                        .aspectRatio(1, contentMode: .fit)
-                        .foregroundStyle(.white)
-                        .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                        .overlay {
-                            VStack {
-                                Image("rate")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .padding(.top)
-                                Text("#52")
-                                    .font(.system(size: 25))
-                            }
-                        }
-                    RoundedRectangle(cornerRadius: 25)
-                        .aspectRatio(1, contentMode: .fit)
-                        .foregroundStyle(.white)
-                        .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                        .overlay {
-                            VStack {
-                                Text("9594")
-                                    .font(.system(size: 25))
-                                Text("Матчи")
-                                    .font(.system(size: 25))
-                            }
-                        }
+                  HeaderRateInformationRectangle()
+                    HeaderInformationRectangle(topText: "9594", bottomText: "Матчи")
                 }
                 VStack {
-                    RoundedRectangle(cornerRadius: 25)
-                        .aspectRatio(1, contentMode: .fit)
-                        .foregroundStyle(.white)
-                        .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                        .overlay {
-                            VStack {
-                                Text("1294")
-                                    .font(.system(size: 25))
-                                Text("MMR")
-                                    .font(.system(size: 25))
-                            }
-                        }
-                    RoundedRectangle(cornerRadius: 25)
-                        .aspectRatio(1, contentMode: .fit)
-                        .foregroundStyle(.white)
-                        .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                        .overlay {
-                            VStack {
-                                Text("51,64%")
-                                    .font(.system(size: 25))
-                                Text("Победы")
-                                    .font(.system(size: 25))
-                            }
-                        }
+               HeaderInformationRectangle(topText: "1294", bottomText: "MMR")
+                HeaderInformationRectangle(topText: "51,64%", bottomText: "Победы")
                 }
             })
             .padding(15)
             .frame(height: 250)
         }
-        
     }
 }
 
-struct NavigationView: View {
+struct HeaderInformationRectangle: View {
+    var topText: String
+    var bottomText: String
     var body: some View {
-        VStack(spacing: -5) {
-            VStack(content: {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.white)
-                    .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                    .frame(height: 50)
-                    .padding(10)
-                    .overlay {
-                        HStack {
-                            Text("Избранные команды")
-                                .font(.system(size: 25))
-                                .padding(.leading)
-                            Spacer()
-                        }
+        RoundedRectangle(cornerRadius: 16)
+            .aspectRatio(1, contentMode: .fit)
+            .foregroundStyle(.white.gradient)
+            .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
+            .overlay {
+                VStack {
+                    VStack {
+                        Text(topText)
+                            .font(.system(size: 25))
+                        Text(bottomText)
+                            .font(.system(size: 20))
                     }
-            })
-            VStack(content: {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.white)
-                    .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                    .frame(height: 50)
-                    .padding(10)
-                    .overlay {
-                        HStack {
-                            Text("Избранные игроки")
-                                .font(.system(size: 25))
-                                .padding(.leading)
-                            Spacer()
-                        }
-                    }
-            })
-            VStack(content: {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.white)
-                    .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                    .frame(height: 50)
-                    .padding(10)
-                    .overlay {
-                        HStack {
-                            Text("Настройки")
-                                .font(.system(size: 25))
-                                .padding(.leading)
-                            Spacer()
-                        }
-                    }
-            })
-            VStack(content: {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.white)
-                    .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                    .frame(height: 50)
-                    .padding(10)
-                    .overlay {
-                        HStack {
-                            Text("О проекте")
-                                .font(.system(size: 25))
-                                .padding(.leading)
-                            Spacer()
-                        }
-                    }
-            })
-            VStack(content: {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.white)
-                    .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
-                    .frame(height: 50)
-                    .padding(10)
-                    .overlay {
-                        HStack {
-                            Text("Выйти из учетной записи")
-                                .font(.system(size: 25))
-                                .padding(.leading)
-                            Spacer()
-                        }
-                    }
-            })
+                }
+            }
+    }
         }
+
+struct HeaderRateInformationRectangle: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 16)
+            .aspectRatio(1, contentMode: .fit)
+            .foregroundStyle(.white.gradient)
+            .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
+            .overlay {
+                VStack {
+                    Image("rate")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.top)
+                    Text("#52")
+                        .font(.system(size: 20))
+                }
+            }
+    }
+}
+
+struct NavigationListView: View {
+    var body: some View {
+        List {
+            ForEach(0 ... 5, id: \.self) { _ in
+                NavigationLink("Избранные команды", destination: {
+                    
+                })
+            }
+        }
+        .scrollContentBackground(.hidden)
+        .scrollDisabled(true)
+        .shadow(color: Color.gray, radius: 10, x: 0, y: 5)
     }
 }
 
